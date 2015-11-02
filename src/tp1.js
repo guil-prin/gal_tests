0ca25ba66c31f955589b4ce1e05e317c4f9eb989
Guillaume Prin <gprin@Salle110>
2015-11-02 14:48:15 +0100

    getGridPositions add et refactoring part 1

--- src/tp1.js
+++ src/tp1.js
@@ -1,60 +1,59 @@
 /**
  * Created by gprin on 01/10/15.
  */
-var Engine = function() {
-    var nbBilles = 0;
-    var currentPlayer = 1; // 1 = Blanc, 2 = Noir
-    var grid = new Array(2);
-    for(var i = 0 ; i < 2 ; i++) {
+var AlreadyPlayedSquareException = function () {
+    "use strict";
+    this.name = "Already played square";
+};
+
+var Engine;
+Engine = function () {
+    "use strict";
+    var nbMarbles = 0, currentPlayer = 1, grid = new Array(2), i, j, k;
+    for (i = 0; i < 2; i++) {
         grid[i] = new Array(2);
-        for(var j = 0 ; j < 3 ; j++) {
+        for (j = 0; j < 3; j++) {
             grid[i][j] = new Array(3);
-            for(var k = 0 ; k < 3 ; k++) {
+            for (k = 0; k < 3; k++) {
                 grid[i][j][k] = new Array(3);
             }
         }
     }
 
-    this.getNbBilles = function() {
-        return nbBilles;
+    this.getNbMarbles = function () {
+        return nbMarbles;
     };
 
-    this.getCurrentPlayer = function() {
+    this.getCurrentPlayer = function () {
         return currentPlayer;
     };
 
-    this.setTokenInGrid = function(square) {
-        var pos1 = (square.charCodeAt(0)) - 97;
-        var pos2 = (square.charCodeAt(1)) - 49;
-        var i1 = Math.floor(pos1/3);
-        var j1 = Math.floor(pos2/3);
-        var i2 = pos1 % 3;
-        var j2 = pos2 % 3;
-        if (grid[i1][j1][i2][j2] == undefined) {
-            grid[i1][j1][i2][j2] = currentPlayer;
-            nbBilles++;
-        }
-        else {
-            console.log("test");
-            throw new alreadyPlayedSquareException();
+    this.setTokenInGrid = function (square) {
+        var pos = this.getGridPositions(square);
+        if (this.getSquare(square) === undefined) {
+            grid[pos.i1][pos.j1][pos.i2][pos.j2] = currentPlayer;
+            nbMarbles++;
+        } else {
+            throw new AlreadyPlayedSquareException();
         }
     };
 
-    this.getSquare = function(square) {
-        var pos1 = (square.charCodeAt(0)) - 97;
-        var pos2 = (square.charCodeAt(1)) - 49;
-        var i1 = Math.floor(pos1/3);
-        var j1 = Math.floor(pos2/3);
-        var i2 = pos1 % 3;
-        var j2 = pos2 % 3;
-        return grid[i1][j1][i2][j2];
+    this.getSquare = function (square) {
+        var pos = this.getGridPositions(square);
+        return grid[pos.i1][pos.j1][pos.i2][pos.j2];
     };
 
-    this.rotateBoard = function(numGridToRotate, rotateWay) {
-        var i1 = Math.floor(numGridToRotate/2);
-        var j1 = numGridToRotate%2;
-        var tmp;
-        if(rotateWay) {
+    this.getGridPositions = function (square) {
+        var pos1 = (square.charCodeAt(0)) - 97, pos2 = (square.charCodeAt(1)) - 49;
+        return {i1 : Math.floor(pos1 / 3),
+            j1 : Math.floor(pos2 / 3),
+            i2 : pos1 % 3,
+            j2 : pos2 % 3};
+    };
+
+    this.rotateBoard = function (numGridToRotate, rotateWay) {
+        var i1 = Math.floor(numGridToRotate / 2), j1 = numGridToRotate % 2, tmp = 0;
+        if (rotateWay) {
             tmp = grid[i1][j1][0][0];
             grid[i1][j1][0][0] = grid[i1][j1][0][2];
             grid[i1][j1][0][2] = grid[i1][j1][2][2];
@@ -65,8 +64,7 @@ var Engine = function() {
             grid[i1][j1][0][1] = grid[i1][j1][1][2];
             grid[i1][j1][1][2] = grid[i1][j1][2][1];
             grid[i1][j1][2][1] = tmp;
-        }
-        else {
+        } else {
             tmp = grid[i1][j1][0][0];
             grid[i1][j1][0][0] = grid[i1][j1][2][0];
             grid[i1][j1][2][0] = grid[i1][j1][2][2];
@@ -79,18 +77,13 @@ var Engine = function() {
             grid[i1][j1][0][1] = tmp;
         }
 
-        if(currentPlayer == 1) {
+        if (currentPlayer === 1) {
             currentPlayer = 2;
-        }
-        else {
+        } else {
             currentPlayer = 1;
         }
-    }
+    };
 
 };
-
-var alreadyPlayedSquareException = function() {
-    this.name = "Already played square";
-}
 
 var board = new Engine();
--- test/test.js
+++ test/test.js
@@ -2,63 +2,63 @@
  * Created by gprin on 01/10/15.
  */
 
-Story = TestCase("Story");
+var Story = TestCase("Story");
 
-Story.prototype.testStoryOne = function() {
-    var nbBilles = board.getNbBilles();
-    assertEquals(0, nbBilles);
+Story.prototype.testStoryOne = function () {
+    var nbMarbles = board.getNbMarbles();
+    assertEquals(0, nbMarbles);
 };
 
-Story.prototype.testStoryTwo = function() {
+Story.prototype.testStoryTwo = function () {
     var currentPlayer = board.getCurrentPlayer();
     assertEquals(1, currentPlayer);
-}
+};
 
-Story.prototype.testStoryThree = function() {
+Story.prototype.testStoryThree = function () {
     var selectedSquare = "a1";
     board.setTokenInGrid(selectedSquare);
-    var caseJouee = board.getSquare(selectedSquare);
-    assertEquals(1, caseJouee);
-}
+    var playedSquare = board.getSquare(selectedSquare);
+    assertEquals(1, playedSquare);
+};
 
-Story.prototype.testStoryFour = function() {
-    var nbBilles = board.getNbBilles();
-    assertEquals(1, nbBilles);
-}
+Story.prototype.testStoryFour = function () {
+    var nbMarbles = board.getNbMarbles();
+    assertEquals(1, nbMarbles);
+};
 
-Story.prototype.testStoryFive = function() {
+Story.prototype.testStoryFive = function () {
     var numGridToRotate = 0;
     var rotateWay = 1;
     board.rotateBoard(numGridToRotate, rotateWay);
-    var caseJouee = board.getSquare("a1");
-    assertEquals(undefined, caseJouee);
-    caseJouee = board.getSquare("c1");
-    assertEquals(1, caseJouee);
-}
+    var playedSquare = board.getSquare("a1");
+    assertEquals(undefined, playedSquare);
+    playedSquare = board.getSquare("c1");
+    assertEquals(1, playedSquare);
+};
 
-Story.prototype.testStorySix = function() {
+Story.prototype.testStorySix = function () {
     var currentPlayer = board.getCurrentPlayer();
     assertEquals(2, currentPlayer);
-}
+};
 
-Story.prototype.testStorySeven = function() {
+Story.prototype.testStorySeven = function () {
     var selectedSquare = "a1";
     board.setTokenInGrid(selectedSquare);
-    var caseJouee = board.getSquare(selectedSquare);
-    assertEquals(2, caseJouee);
-}
+    var playedSquare = board.getSquare(selectedSquare);
+    assertEquals(2, playedSquare);
+};
 
-Story.prototype.testStoryEight = function() {
+Story.prototype.testStoryEight = function () {
     var numGridToRotate = 0;
-    var rotateWay = 0; // 1 = horaire - 0 = antihoraire
+    var rotateWay = 0;
     board.rotateBoard(numGridToRotate, rotateWay);
-    var caseJouee = board.getSquare("a1");
-    assertEquals(1, caseJouee);
-    caseJouee = board.getSquare("a3");
-    assertEquals(2, caseJouee);
-}
+    var playedSquare = board.getSquare("a1");
+    assertEquals(1, playedSquare);
+    playedSquare = board.getSquare("a3");
+    assertEquals(2, playedSquare);
+};
 
-Story.prototype.testStoryNine = function() {
+Story.prototype.testStoryNine = function () {
     var selectedSquare = "a1";
     assertException(function(){board.setTokenInGrid(selectedSquare)}, "Already played square");
-}
+};
